6c0b1371696664de8befd91b2d8b4eb89a233834
Ferdi Ramdhon Nizar <ferdi@axiatadigital.com>
2018-08-07 22:01:35 +0700

    end of tutorial

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 // Setup
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Ejs view engine
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 
+// Mongoose
+mongoose.connect('mongodb://localhost:27017/simple-node-blog');
+
+// Mongoose Schema
+const postSchema = new mongoose.Schema({
+	body: String
+});
+
+// Mongoose Model
+const Post = mongoose.model('Post', postSchema);
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Routes
 app.get('/', (req, res) => {
-	res.render('index');
+	Post.find({}, (err, posts) => {
+		res.render('index', {posts: posts});
+	});
+});
+
+app.post('/addpost', (req, res) => {
+	let postData = new Post(req.body);
+	postData.save().then(result => {
+		res.redirect('/');
+	}).catch(err => {
+		res.status(400).send('Unable to save data');
+	});
 });
 
 // Listen
